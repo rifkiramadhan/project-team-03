9fdb3e962f19d87759c2db04bca9025f4456c793
Nestiannisa <you@example.com>
2022-02-14 13:37:17 +0700

    server

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const routes = require('./routes/index');
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`app listening on port: ${port}`);
