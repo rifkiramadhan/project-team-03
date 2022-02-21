9d414211c9f169503ea7b6ccb17d541af14203dd
saraluthfiani <sara.luthfiani@gmail.com>
2022-02-21 20:56:47 +0700

    bantu rapihin

--- landing-page/src/components/AboutUs.js
+++ landing-page/src/components/AboutUs.js
@@ -3,31 +3,33 @@ import buy from "../assets/img/buy.png";
 import consul from "../assets/img/consul.png";
 import cart from "../assets/img/cart.png";
 import desk from "../assets/img/desk.png";
+// import './AboutUs.css'
 
 function AboutUs() {
   return (
-    <section class="about">
-      <div class="about-heading">
+    <section class="wrapper-grid">
+      <div class="container-text-about">
         <h1>Why Us?</h1>
         <p>
           We are always committed to providing the best for our customers. In
           addition, we also provide facilities such as:
         </p>
       </div>
-      <div class="row align-items-center">
-        <div className="col">
-          <img src={buy} alt="" />
-          <p1>you can join as a medicine seller</p1>
-        </div>
-        <div className="col">
-          <img src={consul} alt="" />
-          <p1>we provide doctor consultation</p1>
-        </div>
-        <div className="col">
-          <img src={cart} alt="" />
-          <p1>always with competitive prices</p1>
-        </div>
-      </div>
+    <div class="container-about">
+      <div class='banner-img'></div>
+      <img src= {buy} alt="" />
+       <p class="description">you can join as a medicine seller</p>
+    </div>
+    <div class="container-about">
+      <div class='banner-img'></div>
+      <img src= {consul} alt="" />
+       <p class="description">we provide doctor consultation</p>
+    </div>
+    <div class="container-about">
+      <div class='banner-img'></div>
+      <img src= {cart} alt="" />
+       <p class="description">always with competitive prices</p>
+    </div>
     </section>
   );
 }
